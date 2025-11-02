a4defccee848bf0d5fa5022c65338465b0e4d003
vqn27 <nguyen6528@gmail.com>
2025-11-02 16:48:02 -0500

    updated gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    spreadsheetId: '1Wrsei4H72bpPTVINrTfBsFCGNEFESDIBfz7VkDJcDgQ',
+    sheetApiKey: 'AIzaSyAoAs8FDaXIXftX8g9qSnbG8s8KFLFHAJI'
+};
